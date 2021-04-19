0fd4f81f32c76b5ddcd43a3f92b44974ebdcf820
angel337 <57143850+angel337@users.noreply.github.com>
2021-04-19 23:16:12 +0100

    Removed commented Kotlin used as reference for translation

--- app/src/main/java/com/example/diaryapp/MainActivity.java
+++ app/src/main/java/com/example/diaryapp/MainActivity.java
@@ -33,8 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-//                bottomNavigationView.background = null
-//                bottomNavigationView.menu.getItem(2).isEnabled = false
+
 
 
     }
